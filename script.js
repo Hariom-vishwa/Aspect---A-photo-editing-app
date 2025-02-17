5b1d8a87abee7c44281a34e529d185a54fa7b192
Hariom-vishwa <hawkgamerhv@gmail.com>
2025-02-17 23:58:42 +0530

    new changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const fileInp = document.querySelector("#fileInput"),
     uploadBtn = document.querySelector("#uploadBtn"),
     previewImg = document.querySelector(".previewImg"),
@@ -27,33 +27,3 @@ function uploadFile() {
 function beforeImg(){
     previewImg.style.filter = "none"
 }
-=======
-const fileInp = document.querySelector("#fileInput"),
-    uploadBtn = document.querySelector("#uploadBtn"),
-    previewImg = document.querySelector(".previewImg"),
-    brightness = document.querySelector("#brightness"),
-    contrast = document.querySelector("#contrast"),
-    sepia = document.querySelector("#sepia"    ),
-    saturation = document.querySelector("#saturation")
-
-function uploadFile() {
-    fileInp.click()
-    fileInp.onchange = () => {
-        const file = fileInp.files[0];
-        if(file){
-            const imgReader = new FileReader();
-            imgReader.onload = (e) =>{
-                previewImg.style.backgroundImage = `url(${e.target.result})`
-            }
-            imgReader.readAsDataURL(file);
-        }
-    }
-}
-
-
-
-
-function beforeImg(){
-    previewImg.style.filter = "none"
-}
->>>>>>> 2ddbd9075f153f58f8a1924cee62f2caf505dd82
